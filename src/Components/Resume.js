2c97fc88f0f1bbf7dfff4db12d6224223e0db659
nathan <nathanpatrick939@gmail.com>
2021-01-31 18:11:07 -0500

    think I got it

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -60,8 +60,7 @@ class Resume extends Component {
 
          <div className="nine columns main-col">
 
-            <p>{skillmessage}
-            </p>
+          
 
 				<div className="bars">
 				   <ul className="skills">
